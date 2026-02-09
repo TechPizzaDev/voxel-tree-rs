3984645611da115d5003811719485582e224193e
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2026-02-09 20:38:06 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,301 @@
+use std::{borrow::Cow, sync::Arc};
+
+use bytemuck::{Pod, Zeroable};
+use glam::{Mat4, Vec3, Vec4};
+use wgpu::{BindGroup, Buffer, BufferUsages, QuerySet, RenderPipeline};
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowId},
+};
+
+#[derive(Clone, Copy, Default, Pod, Zeroable)]
+#[repr(C)]
+struct UniformData {
+    inverse_mvp_matrix: Mat4,
+    time: Vec4,
+}
+
+struct State {
+    window: Arc<Window>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    size: winit::dpi::PhysicalSize<u32>,
+    surface: wgpu::Surface<'static>,
+    surface_format: wgpu::TextureFormat,
+
+    uniform_buffer: Buffer,
+    bind_group: BindGroup,
+    render_pipeline: RenderPipeline,
+    aspect_ratio: f32,
+    current_frame: u64,
+}
+
+impl State {
+    async fn new(window: Arc<Window>) -> State {
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions::default())
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor::default()).await.unwrap();
+
+        let size = window.inner_size();
+
+        let surface = instance.create_surface(window.clone()).unwrap();
+        let caps = surface.get_capabilities(&adapter);
+        let surface_format = caps.formats[0];
+
+        // Load the shaders from disk
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: None,
+            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+        });
+
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: None,
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::VERTEX | wgpu::ShaderStages::FRAGMENT,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+        });
+
+        let uniform_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: size_of::<UniformData>() as u64,
+            usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: uniform_buffer.as_entire_binding(),
+            }],
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: &[&bind_group_layout],
+            immediate_size: 0,
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: Some("vertex_main"),
+                buffers: &[],
+                compilation_options: Default::default(),
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: Some("fragment_main"),
+                compilation_options: Default::default(),
+                targets: &[Some(surface_format.into())],
+            }),
+            primitive: wgpu::PrimitiveState::default(),
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState::default(),
+            multiview_mask: None,
+            cache: None,
+        });
+
+        let mut state = State {
+            window,
+            device,
+            queue,
+            size,
+            surface,
+            surface_format,
+
+            uniform_buffer,
+            bind_group,
+            render_pipeline,
+            aspect_ratio: 1.0,
+            current_frame: 0,
+        };
+
+        // Configure surface for the first time
+        state.configure_surface();
+
+        state
+    }
+
+    fn get_window(&self) -> &Window {
+        &self.window
+    }
+
+    fn configure_surface(&mut self) {
+        let surface_config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: self.surface_format,
+            // Request compatibility with the sRGB-format texture view we‘re going to create later.
+            view_formats: vec![self.surface_format.add_srgb_suffix()],
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            width: self.size.width,
+            height: self.size.height,
+            desired_maximum_frame_latency: 2,
+            present_mode: wgpu::PresentMode::AutoVsync,
+        };
+        self.surface.configure(&self.device, &surface_config);
+        self.aspect_ratio = self.size.width as f32 / self.size.height as f32;
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+
+        self.configure_surface();
+    }
+
+    fn render(&mut self) {
+        self.current_frame += 1;
+
+        let time = self.current_frame as f32 / 60.0;
+
+        let proj_mat = Mat4::perspective_lh(
+            (2.0 * std::f32::consts::PI) / 7.0,
+            self.aspect_ratio,
+            1.0,
+            16.0,
+        );
+
+        let view_mat = Mat4::from_translation(Vec3::new(0., 0., 24.))
+            * Mat4::from_rotation_x(std::f32::consts::PI * 0.4)
+            * Mat4::from_rotation_z(time * 0.25);
+
+        let data = UniformData {
+            inverse_mvp_matrix: (proj_mat * view_mat).inverse(),
+            time: Vec4::zeroed().with_x(time),
+        };
+
+        self.queue
+            .write_buffer(&self.uniform_buffer, 0, bytemuck::bytes_of(&data));
+
+        let surface_texture = self
+            .surface
+            .get_current_texture()
+            .expect("failed to acquire next swapchain texture");
+        let texture_view = surface_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor {
+                // Without add_srgb_suffix() the image we will be working with
+                // might not be "gamma correct".
+                format: Some(self.surface_format.add_srgb_suffix()),
+                ..Default::default()
+            });
+
+        let mut encoder = self.device.create_command_encoder(&Default::default());
+        // Create the renderpass which will clear the screen.
+        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &texture_view,
+                depth_slice: None,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 100.0 / 255.0,
+                        g: 149.0 / 255.0,
+                        b: 237.0 / 255.0,
+                        a: 1.0,
+                    }),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+            multiview_mask: None,
+        });
+
+        // If you wanted to call any drawing commands, they would go here.
+
+        rpass.set_bind_group(0, &self.bind_group, &[]);
+        rpass.set_pipeline(&self.render_pipeline);
+        rpass.draw(0..6, 0..1);
+
+        // End the renderpass.
+        drop(rpass);
+
+        self.queue.submit([encoder.finish()]);
+        self.window.pre_present_notify();
+        surface_texture.present();
+    }
+}
+
+#[derive(Default)]
+struct App {
+    state: Option<State>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+
+        let state = pollster::block_on(State::new(window.clone()));
+        self.state = Some(state);
+
+        window.request_redraw();
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        let state = self.state.as_mut().unwrap();
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                state.render();
+                state.get_window().request_redraw();
+            }
+            WindowEvent::Resized(size) => {
+                // Reconfigures the size of the surface. We do not re-render
+                // here as this event is always followed up by redraw request.
+                state.resize(size);
+            }
+            _ => (),
+        }
+    }
+}
+
+fn main() {
+    // wgpu uses `log` for all of our logging, so we initialize a logger with the `env_logger` crate.
+    //
+    // To change the log level, set the `RUST_LOG` environment variable. See the `env_logger`
+    // documentation for more information.
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+
+    // When the current loop iteration finishes, immediately begin a new
+    // iteration regardless of whether or not new events are available to
+    // process. Preferred for applications that want to render as fast as
+    // possible, like games.
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    // When the current loop iteration finishes, suspend the thread until
+    // another event arrives. Helps keeping CPU utilization low if nothing
+    // is happening, which is preferred if the application might be idling in
+    // the background.
+    // event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
---
+++ src/shader.wgsl
@@ -0,0 +1,123 @@
+struct Uniforms {
+  inverseModelViewProjectionMatrix: mat4x4f,
+  time: vec4f,
+}
+
+@group(0) @binding(0) var<uniform> uniforms: Uniforms;
+
+struct VertexOutput {
+  @builtin(position) Position: vec4f,
+  @location(0) near: vec3f,
+  @location(1) step: vec3f,
+}
+
+override Xray = false;
+override Opaque = true;
+override NumSteps: u32 = select(160u, 256u, Xray);
+
+@vertex
+fn vertex_main(
+    @builtin(vertex_index) VertexIndex: u32
+) -> VertexOutput {
+    var a = -0.99f;
+    var b = 0.99f;
+    var pos = array<vec2f, 4>(
+        vec2(a, b),
+        vec2(a, a),
+        vec2(b, a),
+        vec2(b, b),
+    );
+    var indices = array<i32, 6>(
+        0, 1, 2,
+        0, 2, 3,
+    );
+    var idx = indices[VertexIndex];
+    var xy = pos[idx];
+
+    var near = uniforms.inverseModelViewProjectionMatrix * vec4f(xy, 0f, 1f);
+    var far = uniforms.inverseModelViewProjectionMatrix * vec4f(xy, 1f, 1f);
+    near /= near.w;
+    far /= far.w;
+    return VertexOutput(
+        vec4f(xy, 0f, 1f),
+        near.xyz,
+        (far.xyz - near.xyz) / f32(NumSteps)
+    );
+}
+
+fn sdSphere(p: vec3f, d: f32) -> f32 {
+    return length(p) - d;
+}
+
+fn sdBox(p: vec3f, b: vec3f) -> f32 {
+    let d = abs(p) - b;
+    return min(max(d.x, max(d.y, d.z)), 0.0) + length(max(d, vec3f(0.0)));
+}
+
+fn getVoxel(c: vec3f) -> f32 {
+    //let p = c + 0.5;
+	//let d = min(max(-sdSphere(p, 7.5), sdBox(p, vec3(6.0))), -sdSphere(p, 25.0));
+
+    let p = c + vec3f(0.5);
+    let cutWave = sin(uniforms.time.x * 0.66);
+    let boxCut = sign(cutWave) * sdSphere(p, 7.5 * abs(cutWave));
+    let box = max(boxCut, sdBox(p, vec3(6.0)));
+    let d = min(box, -sdSphere(p, 24.0));
+
+    return d;
+}
+
+@fragment
+fn fragment_main(
+    @location(0) near: vec3f,
+    @location(1) step: vec3f
+) -> @location(0) vec4f {
+    return frag_voxel(near, step);
+}
+
+fn ray_to_tex(pos: vec3f) -> vec3f {
+    return (pos + 1f) * 0.5f;
+}
+
+fn frag_voxel(near: vec3f, step: vec3f) -> vec4f {
+    let rayPos = near;
+    let rayDir = step;
+
+    var mapPos = floor(rayPos);
+    let deltaDist = abs(vec3f(length(rayDir)) / rayDir);
+    let rayStep = sign(rayDir);
+    var sideDist = (sign(rayDir) * (mapPos - rayPos) + (sign(rayDir) * 0.5) + 0.5) * deltaDist;
+
+    var mask: vec3<bool>;
+    var count = 0u;
+    var max = 0u;
+    var i = 0u;
+    for (; i < NumSteps; i++) {
+        let off = (vec3f(sin(uniforms.time.x * 0.2), 0f, 0f) + 1f) * 0.25;
+
+        let s = getVoxel(mapPos); //textureSampleLevel(myTexture, mySampler, uv * 0.5f, 0f).r;
+        let threshold = (sin(uniforms.time.x * 0.5) + 1.0) * 0.5;
+        if s < 0.0 {
+            count += 1u;
+            if (Opaque) {
+                break;
+            }
+        }
+        max += 1u;
+
+        mask = sideDist <= min(sideDist.yzx, sideDist.zxy);
+        sideDist += vec3f(mask) * deltaDist;
+        mapPos += vec3f(mask) * rayStep;
+    }
+
+    if i == NumSteps {
+        return vec4f(vec3f(
+            f32(count) / f32(max), 
+            f32(count) / f32(NumSteps) * 1f, 
+            f32(max) / f32(NumSteps)) * 1.5f, 
+            1f);
+    }
+
+    let color = vec3f(mask) * vec3f(0.5, 1.0, 0.75);
+    return vec4f(color, 1f);
+}
